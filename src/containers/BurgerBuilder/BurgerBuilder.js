34ee7043d860e33f050588a68d5fba23c6c69181
fist22 <hnamanie1@gmail.com>
2020-08-22 11:01:06 +0530

    Adding Layout to the app

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Auxiliary from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Auxiliary>
+                <div>
+                    Buger
+                </div>
+                <div>Cuild Controls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
