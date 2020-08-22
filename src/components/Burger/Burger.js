b17f628d2dd60373b3edb5fbeedc3a4c639ead97
fist22 <hnamanie1@gmail.com>
2020-08-22 12:29:55 +0530

    Adding Burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BugerIngredient from './BurgerIngredient/BurgerIngredient';
+
+
+const burger = (props) => { 
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    return <BugerIngredient key={igKey + i} type={igKey} />
+                });
+        });
+    return (
+        <div className={classes.Burger}>
+            <BugerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BugerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
+
+export default burger;
